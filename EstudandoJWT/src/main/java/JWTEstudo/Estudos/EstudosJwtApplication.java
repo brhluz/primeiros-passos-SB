262260a8e33be9e3b83e110fb6255309236e9894
Mutsubi <brh_lopes@hotmail.com>
2025-03-06 21:15:06 -0300

    JWT

---
+++ EstudandoJWT/src/main/java/JWTEstudo/Estudos/EstudosJwtApplication.java
@@ -0,0 +1,13 @@
+package JWTEstudo.Estudos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosJwtApplication.class, args);
+	}
+
+}
---
+++ EstudandoJWT/src/test/java/JWTEstudo/Estudos/EstudosJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package JWTEstudo.Estudos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EstudosJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
